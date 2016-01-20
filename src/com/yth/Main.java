14981f450fc0034c7ac2952573df24ee6d56250c
ythmilk <1101320653@qq.com>
2016-01-20 12:36:01 +0800

    改正print

--- src/com/yth/Main.java
+++ src/com/yth/Main.java
@@ -3,7 +3,7 @@ package com.yth;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("github第一次提交");
+		System.out.print("github第一次提交");
 		System.out.println("github第二次提交");
 	}
 
