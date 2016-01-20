269deb78c155b6bdd5dc2a90fcb204e60d467705
ythmilk <1101320653@qq.com>
2016-01-20 12:19:45 +0800

    输出github测试2

--- src/com/yth/Main.java
+++ src/com/yth/Main.java
@@ -3,7 +3,8 @@ package com.yth;
 public class Main {
 
 	public static void main(String[] args) {
-System.out.println("github≤‚ ‘");
+		System.out.println("github≤‚ ‘");
+		System.out.println("github≤‚ ‘2");
 	}
 
 }
